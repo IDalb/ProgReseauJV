1596ee974a520f16218fa11216a3810924141d61
GONON Lucas <gononlucas2@gmail.com>
2025-01-28 18:42:58 -0500

    client

--- ClientProject/src/Client.cpp
+++ ClientProject/src/Client.cpp
@@ -1,7 +1,25 @@
 #include <Socket.h>
-
-int main()
+#include <iostream>
+int main(int argc, char* argv[])
 {
-	int socketId = tpSocket::
+    // Validate the parameters
+    if (argc != 2) {
+        printf("usage: %s server-name\n", argv[0]);
+        return 1;
+    }
+	int socketId = tpSocket::socketCreate();
+    tpSocket::socketConnect(socketId, argv[1]);
+
+    tpSocket::socketSend(socketId, "helo");
+    std::string message;
+    do
+    {
+        message = tpSocket::socketReceive(socketId);
+    } while (message != "");
+
+    std::cout << message;
+    std::cin;
+
+    tpSocket::socketClose(socketId);
 	return 0;
 }
