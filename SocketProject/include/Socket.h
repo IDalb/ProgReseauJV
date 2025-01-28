9b14393c7bc0a7a3167d6802d23f97915a4e0b85
IDalb <synfoniz@gmail.com>
2025-01-28 18:46:28 -0500

    Fix merge of POSIX Socket

--- SocketProject/include/Socket.h
+++ SocketProject/include/Socket.h
@@ -16,6 +16,6 @@ namespace tpSocket
 	std::string socketReceive(int socketId);
 		
 	void socketListen(int socketId);
-	void socketBind(int socketId, std::string_view serverAddress);
+	void socketBind(int socketId);
 	int socketAccept(int socketId);
 }
