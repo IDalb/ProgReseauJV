7491320f0e2123ee15e0344a2727dfb53cdf04e1
IDalb <synfoniz@gmail.com>
2025-01-28 17:35:25 -0500

    Add empty methods in Socket_posix.cpp

--- SocketProject/src/Socket_posix.cpp
+++ SocketProject/src/Socket_posix.cpp
@@ -1,3 +1,35 @@
-#ifdef unix
+//#ifdef unix
 
-#endif // unix
+#include "Socket.h"
+#include <string>
+
+void tpSocket::Socket::socketConnect(std::string_view serverAddress) {
+
+}
+
+void tpSocket::Socket::socketDisconnect() {
+
+}
+
+void tpSocket::Socket::socketSend(std::string_view message) {
+
+}
+
+std::string tpSocket::Socket::socketReceive() {
+    return "";
+}
+
+
+void tpSocket::Socket::socketBind() {
+
+}
+
+void tpSocket::Socket::socketListen() {
+
+}
+
+void tpSocket::Socket::socketAccept() {
+
+}
+
+//#endif // unix
