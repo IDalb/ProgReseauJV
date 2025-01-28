41b2d68993d20afff81986135804b1ba6fa489f0
IDalb <synfoniz@gmail.com>
2025-01-28 18:08:24 -0500

    Fix typo in Socket.h

--- SocketProject/include/Socket.h
+++ SocketProject/include/Socket.h
@@ -13,15 +13,15 @@ namespace tpSocket
 		Socket();
 		~Socket();
 		
-		void socketConnect(int sockedId, std::string_view serverAddress);
-		void socketDisconnect(int sockedId);
+		void socketConnect(int socketId, std::string_view serverAddress);
+		void socketDisconnect(int socketId);
 		
-		void socketSend(int sockedId, std::string_view message);
-		std::string socketReceive(int sockedId);
+		void socketSend(int socketId, std::string_view message);
+		std::string socketReceive(int socketId);
 		
-		void socketListen(int sockedId);
-		void socketBind(int sockedId);
-		void socketAccept(int sockedId);
+		void socketListen(int socketId);
+		void socketBind(int socketId);
+		void socketAccept(int socketId);
 	private:
 	};
 }
